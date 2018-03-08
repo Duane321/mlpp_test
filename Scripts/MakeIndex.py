ccb45f93a2732cf3bf52ba886fa7a349a2e70175
Duane Rich <DuaneRich321@gmail.com>
2018-03-07 21:09:36 -0800

    testing indexer

--- Scripts/MakeIndex.py
+++ Scripts/MakeIndex.py
@@ -20,20 +20,27 @@ preamble = """# Machine Learning: A Probabilistic Perspective
 git_loc = 'https://duane321.github.io/mlpp_test/TestPDFs/'
 private_loc = 'https://duane321.github.io/mlpp_test/TestPDFs/'
 
-name_pairings = pd.read_csv('./TestPDFs/name_pairings.csv')
+# name_pairings = pd.read_csv('./PublicPDFs/name_pairings.csv')
+#
+# lines = [preamble]
+#
+# for i in range(name_pairings.shape[0]):
+#     full_name = name_pairings['full_name'].iloc[i]
+#     hashed_name = name_pairings['hashed_name'].iloc[i]
+#     if name_pairings['level'].iloc[i] == 'public':
+#         line = '|[' + full_name + '](' + git_loc + hashed_name + '_ex.pdf)|[Solution](' + git_loc + hashed_name + '_sol.pdf)|'
+#     elif name_pairings['level'].iloc[i] == 'private':
+#         line = '|[' + full_name + '](' + git_loc + hashed_name + '_ex.pdf)|[Solution](' + private_loc + hashed_name + '_sol.pdf)|'
+#     else:
+#         raise ValueError('Incorrect level given for {}'.format(hashed_name))
+#     lines.append(line+'\n')
 
-lines = [preamble]
-
-for i in range(name_pairings.shape[0]):
-    full_name = name_pairings['full_name'].iloc[i]
-    hashed_name = name_pairings['hashed_name'].iloc[i]
-    if name_pairings['level'].iloc[i] == 'public':
-        line = '|[' + full_name + '](' + git_loc + hashed_name + '_ex.pdf)|[Solution](' + git_loc + hashed_name + '_sol.pdf)|'
-    elif name_pairings['level'].iloc[i] == 'private':
-        line = '|[' + full_name + '](' + git_loc + hashed_name + '_ex.pdf)|[Solution](' + private_loc + hashed_name + '_sol.pdf)|'
-    else:
-        raise ValueError('Incorrect level given for {}'.format(hashed_name))
-    lines.append(line+'\n')
-
+lines = ['# Machine Learning: A Probabilistic Perspective\r\n',
+ '\r\n',
+ '| Exercise  | Solution |\r\n',
+ '| ------------- | ------------- |\r\n',
+ '| [Subderivative of the hinge loss function](https://duane321.github.io/mlpp_test/TestPDFs/subgradHinge_ex.pdf)  | [Solution](https://duane321.github.io/mlpp_test/TestPDFs/subgradHinge_sol.pdf)  |\r\n',
+ '| [Reproducing kernel property](https://duane321.github.io/mlpp_test/TestPDFs/reproducing_ex.pdf)  | [Solution](https://duane321.github.io/mlpp_test/TestPDFs/reproducing_sol.pdf)  |\r\n',
+ '| [Orthogonal matrices](https://duane321.github.io/mlpp_test/TestPDFs/orthogonalMatrices_ex.pdf)  | [Solution](https://duane321.github.io/mlpp_test/TestPDFs/orthogonalMatrices_sol.pdf)  |\r\n']
 with open('index.md','w') as file:
     file.write(''.join(lines))
